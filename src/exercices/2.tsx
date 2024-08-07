fdb59bba091caf828729f99f52a71785b236ade3
Tony JEROME <tony.jerome@include-tech.fr>
2024-08-07 15:01:41 +0200

    Hooks exercice done

--- src/exercices/2.tsx
+++ src/exercices/2.tsx
@@ -1,34 +1,103 @@
+import { FormEvent, useRef, useState } from "react";
 import { Board } from "../lib/tictactoe/Board";
 import {
   calculateNextValue,
   calculateStatus,
   getDefaultSquares,
+  SquareValue,
 } from "../lib/tictactoe/helpers";
 
 type GameInfoProps = {
   status: string;
+  userNames: UserNames;
 };
 
-const GameInfo = ({ status }: GameInfoProps) => {
+const GameInfo = ({ status, userNames }: GameInfoProps) => {
   return (
     <div className="game-info">
+      <div className="flex gap-3 center">
+        <span>
+          <b>X</b>:{userNames.X}
+        </span>
+        <span>VS</span>
+        <span>
+          <b>O</b>:{userNames.O}
+        </span>
+      </div>
       <div>{status}</div>
     </div>
   );
 };
 
 const Game = () => {
-  // 🦁 Utilise `useState` pour gérer l'état des cases (attention à l'utiliser correctement) et résout les erreurs TypeScript
-  const squares = getDefaultSquares();
+  const initialSquares: SquareValue[] = getDefaultSquares();
+
+  const [squares, setSquares] = useState(initialSquares);
+  const [userNames, setUserNames] = useState<UserNames>({
+    X: null,
+    O: null,
+  });
 
   const nextValue = calculateNextValue(squares);
   const status = calculateStatus(squares, nextValue);
 
+  if (!userNames.X || !userNames.O) {
+    return (
+      <UserNamesForm
+        onUserNamesSubmitted={(usernames) => setUserNames(usernames)}
+      />
+    );
+  }
+
   return (
     <div className="game">
-      <GameInfo status={status} />
+      <GameInfo status={status} userNames={userNames} />
       <Board squares={squares} />
     </div>
+  );
+};
+
+type UserNamesFormProps = {
+  onUserNamesSubmitted: (user: { X: string; O: string }) => void;
+};
+
+type UserNames = {
+  X: string | null;
+  O: string | null;
+};
+
+const UserNamesForm = ({ onUserNamesSubmitted }: UserNamesFormProps) => {
+  const userXRef = useRef<HTMLInputElement>(null);
+  const userORef = useRef<HTMLInputElement>(null);
+  const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const userX = userXRef.current?.value;
+    const userO = userORef.current?.value;
+
+    if (!userX || !userO) {
+      return;
+    }
+
+    if (userX === userO) {
+      alert("Usernames must be different");
+      return;
+    }
+
+    return onUserNamesSubmitted({
+      X: userX,
+      O: userO,
+    });
+  };
+
+  return (
+    <form onSubmit={onSubmit} className="vertical-stack">
+      <label htmlFor="user1">User X</label>
+      <input id="user1" ref={userXRef} required minLength={2} />
+      <label htmlFor="user2">User O</label>
+      <input id="user2" ref={userORef} required minLength={2} />
+      <button type="submit">Submit</button>
+    </form>
   );
 };
 
